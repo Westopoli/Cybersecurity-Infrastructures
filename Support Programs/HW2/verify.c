04ebed7dd57660fd626115608a671bbcefb394ce
Westopoli <westleyyarlott@gmail.com>
2026-02-04 16:26:13 -0500

    working server.c

---
+++ Support Programs/HW2/verify.c
@@ -0,0 +1,96 @@
+// Pezzle Generation 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/sha.h>
+#include <openssl/evp.h>
+
+int main() {
+    
+    
+
+    return 0;
+}  
+
+
+
+
+
+/*
+    File I/O Functions
+*/
+
+ // Read File
+char* Read_File(const char *filename, int *length) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "Error: Cannot open file %s\n", filename);
+        return NULL;
+    }
+    
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
+    char *buffer = (char*)malloc(file_size + 1);
+    if (!buffer) {
+        fclose(file);
+        return NULL;
+    }
+    
+    size_t read_size = fread(buffer, 1, file_size, file);
+    buffer[read_size] = '\0';
+    
+    // Remove trailing whitespace
+    while (read_size > 0 && (buffer[read_size-1] == '\n' || 
+                              buffer[read_size-1] == '\r' || 
+                              buffer[read_size-1] == ' ')) {
+        buffer[--read_size] = '\0';
+    }
+    
+    *length = read_size;
+    fclose(file);
+    return buffer;
+}
+
+ // Write string to file
+int Write_File(const char *filename, const char *data) {
+    FILE *file = fopen(filename, "w");
+    if (!file) {
+        fprintf(stderr, "Error: Cannot open file %s for writing\n", filename);
+        return -1;
+    }
+    
+    fprintf(file, "%s", data);
+    fclose(file);
+    return 0;
+}
+
+/*
+    Utility Functions
+*/
+
+int Read_Int_From_File(const char *filename) {
+    int length;
+    char *str = Read_File(filename, &length);
+    if (!str) return -1;
+    
+    int value = atoi(str);
+    free(str);
+    return value;
+}
+
+int Write_Int_To_File(const char *filename, int value) {
+    char buffer[32];
+    sprintf(buffer, "%d", value);
+    return Write_File(filename, buffer);
+}
+
+void Print_Hex(const char *label, const unsigned char *data, int len) {
+    printf("%s: ", label);
+    for (int i = 0; i < len; i++) {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+}
